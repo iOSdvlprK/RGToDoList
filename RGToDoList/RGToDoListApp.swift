79f998a2ca40b9141dac38ad8c8f9f91f76c1850
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-03-26 23:42:20 +0900

    create xcode project

---
+++ RGToDoList/RGToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGToDoListApp.swift
+//  RGToDoList
+//
+//  Created by joe on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct RGToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
