69757429f7cb2105042c31ef5c3b1570a5f22d9a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-05-07 00:10:34 +0900

    set up project structure and folder hierarchy; integrate theme assets
    - move ContentView to Core/AppStartingView.swift and update struct name
    - add custom color sets to Colors.xcassets for the design system
    - update AppIcon with AllDone.png

--- RGToDoList/Core/AppStartingView.swift
+++ RGToDoList/Core/AppStartingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  AppStartingView.swift
 //  RGToDoList
 //
 //  Created by joe on 3/26/26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AppStartingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    AppStartingView()
 }
--- RGToDoList/RGToDoListApp.swift
+++ RGToDoList/RGToDoListApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RGToDoListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppStartingView()
         }
     }
 }
